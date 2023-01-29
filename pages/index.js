7a217ce1cb83c0766148a6e4ac315a239b1be973
Jonathan Lagneaux <lagneaux.j@gmail.com>
2023-01-29 14:09:59 +0100

    feat: setup gosunet model

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,100 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState, useEffect } from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+import buildspaceLogo from '../assets/buildspace-logo.png'
 
 const Home = () => {
+  const maxRetries = 20
+  const [input, setInput] = useState('knight character with blue eyes, intricate, beautiful, 8k resolution, dynamic lighting, hyperdetailed, quality 3D rendered, volumetric lighting, greg rutkowski, detailed background, artstation character portrait, dnd character portrait')
+  const [img, setImg] = useState('')
+  // Numbers of retries
+  const [retry, setRetry] = useState(0)
+  // Number of retries left
+  const [retryCount, setRetryCount] = useState(maxRetries)
+  const [isGenerating, setIsGenerating] = useState(false)
+  const [finalPrompt, setFinalPrompt] = useState('');
+  const onChange = (event) => {
+    setInput(event.target.value)
+  }
+  const generateAction = async () => {
+    console.log('Generating...')
+
+    if (isGenerating && retry === 0) return
+
+    // Set loading has started
+    setIsGenerating(true)
+
+    // If this is a retry request, take away retryCount
+    if (retry > 0) {
+      setRetryCount((prevState) => {
+        if (prevState === 0) {
+          return 0
+        } else {
+          return prevState - 1
+        }
+      })
+
+      setRetry(0)
+    }
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'image/jpeg',
+      },
+      body: JSON.stringify({ input }),
+    })
+
+    const data = await response.json()
+
+    if (response.status === 503) {
+      setRetry(data.estimated_time);
+      return;
+    }
+
+    if (!response.ok) {
+      console.log(`Error: ${data.error}`);
+      setIsGenerating(false);
+      return;
+    }
+
+    // Set final prompt here
+    setFinalPrompt("Close up portrait of Gosunet as a " + input);
+    setImg(data.image)
+    setIsGenerating(false)
+  }
+
+  const sleep = (ms) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  };
+  
+
+  // Add useEffect here
+  useEffect(() => {
+    const runRetry = async () => {
+      if (retryCount === 0) {
+        console.log(
+          `Model still loading after ${maxRetries} retries. Try request again in 5 minutes.`,
+        )
+        setRetryCount(maxRetries)
+        return
+      }
+
+      console.log(`Trying again in ${retry} seconds.`)
+
+      await sleep(retry * 1000)
+
+      await generateAction()
+    }
+
+    if (retry === 0) {
+      return
+    }
+
+    runRetry()
+  }, [retry])
+
   return (
     <div className="root">
       <Head>
@@ -11,12 +103,41 @@ const Home = () => {
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>your generator one-liner</h1>
+            <h1>Gosunet image generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>description of your generator</h2>
+            <h2>
+              Turn me into anyone you want! Close up portrait of Gosunet as a :
+            </h2>
+          </div>
+          <div className="prompt-container">
+            <input className="prompt-box" value={input} onChange={onChange} />
+            <div className="prompt-buttons">
+              <a
+                className={
+                  isGenerating ? 'generate-button loading' : 'generate-button'
+                }
+                onClick={generateAction}
+              >
+                {/* Tweak to show a loading indicator */}
+                <div className="generate">
+                  {isGenerating ? (
+                    <span className="loader"></span>
+                  ) : (
+                    <p>Generate</p>
+                  )}
+                </div>
+              </a>
+            </div>
           </div>
         </div>
+        {/* Add output container */}
+        {img && (
+          <div className="output-content">
+            <Image src={img} width={512} height={512} alt={input} />
+            <p>{finalPrompt}</p>
+          </div>
+        )}
       </div>
       <div className="badge-container grow">
         <a
@@ -31,7 +152,7 @@ const Home = () => {
         </a>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
